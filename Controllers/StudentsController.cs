b57d6d7f0e54128f4b1e651e79f676c77a1fcb77
TinaafWeb <88359480+TinaafWeb@users.noreply.github.com>
2021-11-23 23:06:34 -0500

    StudentsController updated

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -9,8 +9,11 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lab6.Controllers
-{
-    public class StudentsController : Controller
+{ 
+    [ApiController]
+    [Route("[controller]")]
+
+    public class StudentsController : ControllerBase
     {
         private readonly StudentDbContext _context;
 
@@ -18,11 +21,12 @@ namespace Lab6.Controllers
         {
             _context = context;
         }
+        
         //Get: Students
         /// <summary>
         /// Get collection of Students.
         /// </summary>
-        /// <returns>A collection of Students</returns>
+        /// <returns> A collection of Students </returns>
         /// <response code="200">Returns a collection of Students</response>
         /// <response code="500">Internal error</response>      
         [HttpGet]
@@ -69,7 +73,7 @@ namespace Lab6.Controllers
         ///     {
         ///        "FirstName": "fname",
         ///        "LastName": "lname",
-        ///        "Program": "program",
+        ///        "Program": "program"
         ///     }
         ///
         /// </remarks>
@@ -87,14 +91,13 @@ namespace Lab6.Controllers
             {
                 FirstName = studentBase.FirstName,
                 LastName = studentBase.LastName,
-                Program = studentBase.Program,
+                Program = studentBase.Program
             };
 
             _context.Add(student);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = student.ID }, student);
         }
-
 
         // PUT: Students/5
         /// <summary>
@@ -105,21 +108,21 @@ namespace Lab6.Controllers
         ///
         ///     PUT /Students
         ///     {
-        ///        "FirstName": "Maryam",
-        ///        "LastName": "Afshar",
-        ///        "Program": "CET"
+        ///        "FirstName": "fname",
+        ///        "LastName": "lname",
+        ///        "Program": "program"
         ///     }
         ///
         /// </remarks>
         /// <param id="id"></param>
         /// <returns>An updated Student</returns>
         /// <response code="200">Returns the updated Student</response>
-        /// <response code="404">If the Resource does not exist </response>
+        /// <response code="201">Returns the newly created Student</response>
         /// <response code="400">If the Student or id is malformed</response>      
         /// <response code="500">Internal error</response>
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Student>> Update(Guid id, [Bind("FirstName,LastName,Program")] StudentBase studentBase)
@@ -128,12 +131,15 @@ namespace Lab6.Controllers
             {
                 FirstName = studentBase.FirstName,
                 LastName = studentBase.LastName,
-                Program = studentBase.Program,
+                Program = studentBase.Program
             };
 
             if (!StudentExists(id))
             {
-                return NotFound();
+                student.ID = id;
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetById), new { id = student.ID }, student);
             }
 
             Student dbStudent = await _context.Students.FindAsync(id);
